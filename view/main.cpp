b1c3f8b6bd3c8a54c0f9a8ee8eaffcc278881987
Aryel <aryelpenido@gmail.com>
2019-10-28 22:24:50 -0300

    makefile

--- view/main.cpp
+++ view/main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-#include <Turma.h>
+#include "../modelo/entidade/Turma.hpp"
 #include "../controle/ControleAluno.hpp"
-#include "../controle/ControleProfessor.h"
-#include "../controle/ControleTurma.h"
+#include "../controle/ControleProfessor.hpp"
+#include "../controle/ControleTurma.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -11,6 +11,8 @@ int main(int argc, char const *argv[])
     ControleAluno *cc = new ControleAluno();
     cc->inserecontrole("Aryel","Jaoa","Michelle","13/12/1999","Rua rio grande 601, novo riacho,contagem,minas gerais,brasil");
     cc->inserecontrole("Maria","Jaoa2","Michelle2","21/12/2003","Rua rio grande 601, novo riacho,contagem,minas gerais,brasil2");
+    cc->inserecontrole("joaozinho","Jaoa3","Michelle3","21/12/2011","Rua rio grande 601, novo riacho,contagem,minas gerais,brasil2");
+    //cc->ListaAlunos();
     //cc->ListaAlunos();
     vector <Aluno*> vv = cc->getListaAlunos();
 
@@ -26,11 +28,14 @@ int main(int argc, char const *argv[])
     Professor prof = pp->getProfessor(nomeprof);
     ct->insere(prof,vv,codt,"2019");
     ct->Lancarnotas();
-    cc->ListaAlunos();
+   // cc->ListaAlunos();
+   // pp->ListarProfessor();
+   // ct->Lancarnotas();
+    //cc->ListaAlunos();
+    ct->Boletim();
 
 
 
- //Professor *p1 = new Professor("AAAAA","Florestal","Exatas",200);
 
     return 0;
 }
